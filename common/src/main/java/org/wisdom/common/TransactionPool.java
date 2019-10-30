adc58db9e6c5c406031d9ec67d54cc106cc8df04
Sal <m6567fc@outlook.com>
2019-10-30 15:01:59 +0800

    Update TransactionPool.java

--- common/src/main/java/org/wisdom/common/TransactionPool.java
+++ common/src/main/java/org/wisdom/common/TransactionPool.java
@@ -1,12 +1,15 @@
 package org.wisdom.common;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface TransactionPool {
 
+    // collect transactions into transaction pool
     void collect(Transaction... transactions);
 
-    List<Transaction> pop();
+    // pop a transaction from pool
+    Optional<Transaction> pop();
 
+    // get size of current transaction pool
     int size();
 }
