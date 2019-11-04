b25a9eb68e6f47329b62742d5166185877b52004
Sal <m6567fc@outlook.com>
2019-11-04 13:58:18 +0800

    add lru state factory

---
+++ common/src/main/java/org/wisdom/common/LRUStateFactory.java
@@ -0,0 +1,62 @@
+package org.wisdom.common;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.Optional;
+
+public class LRUStateFactory<T extends State<T>> {
+    private static final long MAXIMUM_TRAVERSE_DEPTH = 2048;
+
+    private Cache<String, T> cache;
+
+    private BlockRepository repository;
+
+    private T genesisState;
+
+    public LRUStateFactory(long size, T genesis, BlockRepository repository) {
+        this.cache = CacheBuilder.newBuilder()
+                .maximumSize(size).build();
+        cache.put(genesis.where().toString(), genesis);
+        this.repository = repository;
+    }
+
+    private Optional<T> getFromCache(Block block, long depth) {
+        if (depth > MAXIMUM_TRAVERSE_DEPTH) return Optional.empty();
+        if (block.getHeight() == 0) {
+            return Optional.of(genesisState);
+        }
+        String key = block.getHash().toString();
+        if (cache.asMap().containsKey(key)) {
+            return Optional.of(cache.asMap().get(key));
+        }
+        Optional<Block> o = repository.getBlock(block.getHashPrev().getBytes());
+        if (!o.isPresent()) return Optional.empty();
+
+        Optional<T> parentStateCopied =
+                repository
+                        .getBlock(block.getHashPrev().getBytes())
+                        .flatMap(b -> getFromCache(b, depth + 1).map(State::clone));
+        if (!parentStateCopied.isPresent()) return parentStateCopied;
+        T s = parentStateCopied.get();
+        for (Transaction t : block.getBody()) {
+            try {
+                s.update(block, t);
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }
+        cache.asMap().put(s.where().toString(), s);
+        return Optional.of(s);
+    }
+
+    public Optional<T> get(Block block) {
+        if (!repository.hasBlock(block.getHash().getBytes())) {
+            return Optional.empty();
+        }
+        if (block.getHeight() == 0) {
+            return Optional.of(genesisState);
+        }
+        return getFromCache(block, 0);
+    }
+}
--- common/src/main/java/org/wisdom/common/State.java
+++ common/src/main/java/org/wisdom/common/State.java
@@ -4,4 +4,5 @@ import org.wisdom.exception.StateUpdateException;
 
 public interface State<T> extends Cloneable<T>{
     void update(Block b, Transaction t) throws StateUpdateException;
+    HexBytes where();
 }
