82472ae510e09cb27e4e8e4f16a33ac936991c75
Sal <m6567fc@outlook.com>
2019-11-06 14:52:35 +0800

    parse properties as map

--- consortium/src/main/java/org/wisdom/consortium/GlobalConfig.java
+++ consortium/src/main/java/org/wisdom/consortium/GlobalConfig.java
@@ -3,9 +3,9 @@ package org.wisdom.consortium;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
+import java.util.HashMap;
 
 @ConfigurationProperties(prefix = "consortium")
 @Component
-public class GlobalConfig extends Properties {
+public class GlobalConfig extends HashMap {
 }
