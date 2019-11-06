d255783e7ccf2e77d987c77544daed4617f0a0ae
liuyong199311080810 <1991595613@qq.com>
2019-11-06 09:39:03 +0800

    add gmhelper

--- crypto/src/main/java/org/wisdom/keystore/ed25519/KeyPair.java
+++
@@ -1,7 +0,0 @@
-package org.wisdom.keystore.ed25519;
-
-
-public interface KeyPair {
-    PrivateKey getPrivateKey();
-    PublicKey getPublicKey();
-}
--- crypto/src/main/java/org/wisdom/keystore/ed25519/PrivateKey.java
+++
@@ -1,8 +0,0 @@
-package org.wisdom.keystore.ed25519;
-
-import org.wisdom.keystore.crypto.CryptoException;
-
-public interface PrivateKey extends java.security.PrivateKey {
-    byte[] sign(byte[] msg) throws CryptoException;
-    PublicKey generatePublicKey();
-}
